aac0bb7ecabd8eb6a9084d3f5cdc845fbad314b6
Azania-Dev <azaniam04@gmail.com>
2022-03-21 23:39:13 +0200

    fixed the styling

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,20 +1,14 @@
 import React from "react";
 import "./Expenses.css";
-import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 
 //props is an object that contains all the information we have on our app
 // where ever you are going to use the props, in your component you must include it as a parameter in your function and dont forget the key's
-const Expenses = ({nameOfProduct, price, date}) => {
+const Expenses = ({ nameOfProduct, price, date }) => {
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        nameOfProduct={nameOfProduct}
-        price={price}
-        date={date}
-      />
-    
-    </Card>
+    <div className="expenses">
+      <ExpenseItem nameOfProduct={nameOfProduct} price={price} date={date} />
+    </div>
   );
 };
 
