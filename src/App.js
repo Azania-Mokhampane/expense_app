bc8417f858391b1ac8b2fcb5898752d41623174b
Azania Dev <azaniam04@gmail.com>
2022-03-30 17:46:54 +0200

    passing data from child to parent

--- src/App.js
+++ src/App.js
@@ -4,33 +4,36 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpenses/NewExpenses";
 import "./index.css";
 
-//this is an array(data type) that contains objects with the properties that wil be diplayed on the site
-const expenses = [
-  {
-    id: "p1",
-    title: "Hoody",
-    amount: 500,
-    date: new Date(2022, 1, 14),
-  },
-  {
-    id: "p2",
-    title: "Socks",
-    amount: 120,
-    date: new Date(2022, 2, 12),
-  },
-  {
-    id: "p3",
-    title: "Black T's",
-    amount: 250,
-    date: new Date(2022, 2, 28),
-  },
-];
-
 const App = () => {
+  //this is an array(data type) that contains objects with the properties that wil be diplayed on the site
+  const expenses = [
+    {
+      id: "p1",
+      title: "Hoody",
+      amount: 500,
+      date: new Date(2022, 1, 14),
+    },
+    {
+      id: "p2",
+      title: "Socks",
+      amount: 120,
+      date: new Date(2022, 2, 12),
+    },
+    {
+      id: "p3",
+      title: "Black T's",
+      amount: 250,
+      date: new Date(2022, 2, 28),
+    },
+  ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("In App Js : ", expense);
+  };
   return (
     <>
       <div className="header">Online Expense Tracker App</div>
-      <NewExpense />
+      <NewExpense onSaveExpense={addExpenseHandler} />
       {expenses.map((items) => (
         <Expenses
           nameOfProduct={items.title}
