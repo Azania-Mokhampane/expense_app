a6b4f444234032def27701970328df0bb4fc13b5
Azania Dev <azaniam04@gmail.com>
2022-04-03 19:39:49 +0200

    renamed files

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -1,18 +1,19 @@
-import React from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 
+interface Props {
+  price: string;
+  date: number;
+  name: string;
+}
+
 //props is an object that contains all the information we have on our app
 // where ever you are going to use the props, in your component you must include it as a parameter in your function and dont forget the key's
-const Expenses = (props) => {
+const Expenses = (props: Props) => {
   return (
     <>
       <div className="expenses">
-        <ExpenseItem
-          nameOfProduct={props.name}
-          price={props.price}
-          date={props.date}
-        />
+        <ExpenseItem name={props.name} price={props.price} date={props.date} />
       </div>
     </>
   );
