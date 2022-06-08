59efd79e1fe4743f49131c6f62e637469503a8c6
Azania Dev <azaniam04@gmail.com>
2022-06-08 22:20:41 +0200

    feat: login-form

---
+++ src/components/Login/loginpage.tsx
@@ -0,0 +1,147 @@
+import React, { useEffect, useState } from "react";
+import { Box } from "./loginstyles";
+
+import {
+  Stack,
+  Heading,
+  Container,
+  Input,
+  Button,
+  Text,
+  SimpleGrid,
+} from "@chakra-ui/react";
+type ILOGIN = {
+  LoggenIn: React.Dispatch<React.SetStateAction<boolean>>;
+};
+const Login = (props: ILOGIN) => {
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState({
+    value: "",
+    error: "",
+  });
+  const [userPassword, setUserPassword] = useState("");
+
+  const emailValidator = (email: string) => {
+    const re = /\S+@\S+\.\S+/;
+    if (!email) return "Email can't be empty.";
+    if (!re.test(email))
+      return "Email entered not valid. Please enter a valid email.";
+    return "";
+  };
+
+  const emailError = emailValidator(userEmail.value);
+
+  const validation = userPassword.length < 5 || userName.length < 5;
+
+  const SubmitHandler = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    setUserEmail({ ...userEmail, value: "" });
+
+    if (emailError) {
+      setUserEmail({ ...userEmail, error: emailError });
+      return;
+    }
+    props.LoggenIn(true);
+  };
+
+  console.log("Check Error ", emailError);
+  return (
+    <>
+      <Box>
+        <Container
+          as={SimpleGrid}
+          maxW={"7xl"}
+          columns={{ base: 1, md: 2 }}
+          spacing={{ base: 10, lg: 32 }}
+          py={{ base: 10, sm: 20, lg: 32 }}
+        >
+          <Stack spacing={{ base: 10, md: 20 }}>
+            <Heading
+              lineHeight={1.1}
+              fontSize={{ base: "3xl", sm: "4xl", md: "5xl", lg: "6xl" }}
+            >
+              Keep an accurate record of your personal or business expenses.
+            </Heading>
+          </Stack>
+          <Stack
+            bg={"gray.50"}
+            rounded={"xl"}
+            p={{ base: 4, sm: 6, md: 8 }}
+            spacing={{ base: 8 }}
+            maxW={{ lg: "lg" }}
+          >
+            <Stack spacing={4}>
+              <Heading
+                color={"gray.800"}
+                lineHeight={1.1}
+                fontSize={{ base: "2xl", sm: "3xl", md: "4xl" }}
+              >
+                Login
+              </Heading>
+            </Stack>
+            <form onSubmit={SubmitHandler} style={{ marginTop: 10 }}>
+              <Stack spacing={4}>
+                <Input
+                  type={"text"}
+                  value={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+                  placeholder="Firstname"
+                  bg={"gray.100"}
+                  border={0}
+                  color={"gray.500"}
+                  _placeholder={{
+                    color: "gray.500",
+                  }}
+                />
+                <Input
+                  // type={"email"}
+                  value={userEmail.value}
+                  onChange={(e) =>
+                    setUserEmail({ ...userEmail, value: e.target.value })
+                  }
+                  placeholder="Email"
+                  bg={"gray.100"}
+                  border={0}
+                  color={"gray.500"}
+                  _placeholder={{
+                    color: "gray.500",
+                  }}
+                />
+                <Text style={{ color: "red" }}>{userEmail.error}</Text>
+                <Input
+                  value={userPassword}
+                  type={"password"}
+                  onChange={(e) => setUserPassword(e.target.value)}
+                  placeholder="Password"
+                  bg={"gray.100"}
+                  border={0}
+                  color={"gray.500"}
+                  _placeholder={{
+                    color: "gray.500",
+                  }}
+                />
+              </Stack>
+              <Button
+                type={"submit"}
+                disabled={validation}
+                fontFamily={"heading"}
+                mt={8}
+                w={"full"}
+                bgGradient="linear(to-r, purple.400,blue.400)"
+                color={"white"}
+                _hover={{
+                  bgGradient: "linear(to-r, red.400,pink.400)",
+                  boxShadow: "xl",
+                }}
+              >
+                Login
+              </Button>
+            </form>
+          </Stack>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+export default Login;
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,12 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
 //we use React Dom to fetch our html tags
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
+  document.getElementById("root")
+);
 // and the above code is how call it, the render method taking two parameters that actually linkes the html page with the <App /> component which is our main componet where all the components are linked to
